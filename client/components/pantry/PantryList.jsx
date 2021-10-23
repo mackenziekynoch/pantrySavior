f1329f6154565fa1d563b7836ef4f0b76ca78536
mackenziekynoch <mackenziekynoch@gmail.com>
2021-10-22 21:27:42 -0400

    styled button to add items

--- client/components/pantry/PantryList.jsx
+++ client/components/pantry/PantryList.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+
 import PantryItem from './PantryItem.jsx';
 
-const PantryList = (props) => (
-  <main id='pantryList' className='container-main'>
-    {props.items.map(item =>
-      <PantryItem key={item.id} item={item} />
-    )}
-  </main>
-);
+const PantryList = (props) => {
+
+  return (
+    <main id='pantryList' className='container-main'>
+      {props.items.map(item =>
+        <PantryItem key={item.id} item={item} />
+      )}
+      <button id='addPantryItem' className='circle-button' onClick={() => console.log('adding!')}>Add</button>
+    </main>
+  );
+};
 
 export default PantryList;
